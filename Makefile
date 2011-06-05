3a641cde3d3ef55ed0f2e84b2df71b097543ce47
M. Nunberg <mordy@mpb.local>
2011-06-05 18:51:44 +0100

    some more touches

--- Makefile
+++
@@ -1,4 +0,0 @@
-all: logtest
-
-logtest: logtest.c yolog.c mlog_test.c
-	$(CC) -o $@ $^ -lcurses
--- tests/logtest.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "yolog.h"
-YOLOG_STATIC_INIT(__FILE__, YOLOG_DEBUG);
-int main(void) {
-	yolog_debug("Hello World");
-	do_something();
-	return 0;
-}
